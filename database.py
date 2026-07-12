cae38351fdfef5ff49558bddc18bf83456d36a27
atulonline110 <atulkarverma1122@gmail.com>
2026-07-12 19:29:33 +0530

    Update database.py

--- database.py
+++ database.py
@@ -170,6 +170,14 @@ def register_user(username, password):
             cursor.execute(
                 "INSERT INTO users(username, password) VALUES(?, ?)",
                 (username, password)
+def login_user(username, password):
+    with sqlite3.connect(DATABASE_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT * FROM users WHERE username=? AND password=?",
+            (username, password)
+        )
+        return cursor.fetchone()
             )
             conn.commit()
             return True
